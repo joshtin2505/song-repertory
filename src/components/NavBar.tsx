6dbfa13afa6f00b23e72c89f87018016729535b2
joshtin2505 <justincastro2505@gmail.com>
2024-05-26 00:01:54 -0500

    refactor: Update NavBar component to include responsive menu button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,19 +20,10 @@ export function NavBar() {
             Song Repertory
           </span>
         </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <FaBars className="w-6 h-6" />
-        </button>
+
         <ul
           id="navbar-default"
-          className="hidden md:flex w-auto font-medium  flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700"
+          className="hidden lg:flex w-auto font-medium flex-col p-4 md:p-0 mt-4 border border-neutral-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-neutral-700"
         >
           <li>
             <Link
@@ -46,7 +37,7 @@ export function NavBar() {
           <li>
             <Link
               href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               About
             </Link>
@@ -54,7 +45,7 @@ export function NavBar() {
           <li>
             <Link
               href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Services
             </Link>
@@ -62,7 +53,7 @@ export function NavBar() {
           <li>
             <Link
               href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Pricing
             </Link>
@@ -70,7 +61,7 @@ export function NavBar() {
           <li>
             <Link
               href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-700 md:p-0 dark:text-white md:dark:hover:text-neutral-500 dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Contact
             </Link>
@@ -88,6 +79,16 @@ export function NavBar() {
             className="transition text-white bg-neutral-700 hover:bg-neutral-800 focus:ring-4 focus:ring-neutral-300 font-medium rounded px-4 py-1 dark:bg-white/90 dark:text-neutral-950 dark:hover:bg-white/50 focus:outline-none dark:focus:ring-neutral-800"
           >
             Sign In
+          </button>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-neutral-500 rounded-lg lg:hidden hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <FaBars className="w-6 h-6" />
           </button>
         </div>
       </nav>
