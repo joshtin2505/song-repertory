220d07e658b604c48ab92064dc1e3a0dc4b5524a
joshtin2505 <justincastro2505@gmail.com>
2024-05-26 17:31:42 -0500

    feat: Add responsive screen sizes to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,37 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    screens: {
+      'max-2xl': { max: '1400px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'max-xl': { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'max-lg': { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'max-md': { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'max-sm': { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      '2xl': '1400px',
+      // => @media (min-width: 1536px) { ... }
+
+      xl: '1279px',
+      // => @media (min-width: 1279px) { ... }
+
+      lg: '1023px',
+      // => @media (min-width: 1023px) { ... }
+
+      md: '767px',
+      // => @media (min-width: 767px) { ... }
+
+      sm: '639px'
+      // => @media (min-width: 639px) { ... }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
