2ffc0c6ecfd9e9cabddb1bebeb4fa4b1d1be5fe2
joshtin2505 <justincastro2505@gmail.com>
2024-05-25 23:50:44 -0500

    refactor: Update Features component to use English language data

--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { english, español } from '@/mocks/Home.json'
 function Features() {
-  const { features } = español.featuresSection
+  const { features } = english.featuresSection
   return (
     <section className="bg-gradient-to-br to-neutral-300 from-white dark:bg-gradient-to-br  dark:to-neutral-900 dark:from-neutral-700">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-neutral-900 dark:text-white">
-            Designed for you
+            {english.featuresSection.title}
           </h2>
           <p className="text-neutral-500 sm:text-xl dark:text-neutral-400">
-            At Song Repertory, we create tools to simplify the management of
-            your musical repertoire, facilitating organization and collaboration
-            for musicians and bands.
+            {english.featuresSection.description}
           </p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
