d8d41d555cdef30335d2b4ae3da672d6ff817d20
joshtin2505 <justincastro2505@gmail.com>
2024-05-31 15:54:07 -0500

    refactor: Update CTA component with grayscale images and lazy loading for better performance

--- src/components/Home/CTA.tsx
+++ src/components/Home/CTA.tsx
@@ -1,50 +1,38 @@
 import Image from 'next/image'
-import React from 'react'
-
+import { english } from '@/mocks/Home.json'
+import { FaArrowRight } from 'react-icons/fa6'
 function CTA() {
+  const { CTA } = english
   return (
-    <section className="bg-white dark:bg-gradient-to-b from-black to-neutral-950 min-h-screen">
+    <section className="bg-white dark:bg-gradient-to-b from-black to-neutral-950 min-h-screen flex justify-center">
       <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <Image
           width={1000}
           height={1000}
-          className="w-full dark:hidden grayscale"
+          className="w-full dark:hidden grayscale shadow-2xl"
           src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
           alt="dashboard image"
         />
         <Image
           width={1000}
           height={1000}
-          className="w-full hidden dark:block grayscale "
+          className="w-full hidden dark:block grayscale shadow-2xl shadow-neutral-700"
           src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
           alt="dashboard image"
         />
         <div className="mt-4 md:mt-0">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Let&apos;s create more tools and ideas that brings us together.
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-neutral-900 dark:text-white">
+            {CTA.title}
           </h2>
-          <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-            Flowbite helps you connect with friends and communities of people
-            who share your interests. Connecting with your friends and family as
-            well as discovering new ones is easy with features like Groups.
+          <p className="mb-6 font-light text-neutral-500 md:text-lg dark:text-neutral-400">
+            {CTA.description}
           </p>
           <a
-            href="#"
-            className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
+            href="/register"
+            className="inline-flex items-center transition-colors text-white bg-neutral-700 dark:bg-white dark:hover:bg-white/70 dark:text-black hover:bg-neutral-800 focus:ring-4 focus:ring-neutral-300 font-medium rounded text-sm px-5 py-2.5 text-center dark:focus:ring-neutral-900"
           >
-            Get started
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            {CTA.button}
+            <FaArrowRight className="w-5 h-5 ml-2" />
           </a>
         </div>
       </div>
