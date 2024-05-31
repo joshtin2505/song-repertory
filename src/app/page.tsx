675346f5690207a460420f178b7393a9aee3d79b
joshtin2505 <justincastro2505@gmail.com>
2024-05-31 01:01:21 -0500

    refactor: Add CursorFollower component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+'use client'
 import NavBar from '@/components/NavBar'
 import Hero from '@/components/Home/Hero'
 import Features from '@/components/Home/Features'
 import Pricing from '@/components/Home/pricing'
 import Footer from '@/components/Footer'
+import CursorFollower from '@/components/CursorFollower'
 
 export default function Home() {
   return (
     <main>
+      <CursorFollower />
       <NavBar />
       <Hero />
       <Features />
