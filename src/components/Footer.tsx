94ed6fc3f695231ce55479320e3f678a57c94a64
joshtin2505 <justincastro2505@gmail.com>
2024-06-04 23:08:16 -0500

    refactor: Update Footer component layout for responsive design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <footer className="p-4 bg-white md:p-8 lg:p-10 border-gray-200 dark:bg-black/50 backdrop-blur backdrop-saturate-150 border-t dark:border-white/30">
+    <footer className="p-4 bg-white md:p-8 lg:p-10 border-gray-200 dark:bg-black/50 backdrop-blur backdrop-saturate-150 border-t dark:border-white/30 ">
       <div className="mx-auto max-w-screen-xl text-center flex items-center flex-col">
-        <header className="flex items-center gap-2">
+        <header className="flex items-center gap-2 max-md:flex-col max-md:mt-3">
           <a
             href="#"
             className=" flex gap-2 justify-center items-center text-2xl font-semibold text-neutral-900 dark:text-white"
@@ -16,8 +16,8 @@ function Footer() {
             <LogoDark className="hidden dark:block" width={40} height={40} />
             Song Repertory
           </a>
-          <hr className="rotate-90 w-5" />
-          <p className="my-6 max-w-[600px] text-start text-neutral-500 dark:text-neutral-400">
+          <hr className="rotate-90 w-5 max-md:hidden" />
+          <p className="my-6 max-w-[600px] max-md:text-center text-start text-neutral-500 dark:text-neutral-400">
             {english.featuresSection.description}
           </p>
         </header>
